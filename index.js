43f80d8cbb3a06b3bfad2d9ac64622c52196a974
Viva Laas <vivalaas@udel.edu>
2020-09-08 15:29:29 -0400

    interactivity portion

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 $(function(){
     //this code runs after page is fully loaded
-    
+    $("#go").on("click", function(event){
+        event.preventDefault();
+        $("#bananas").text($("#fname").val() + " " + $("#lname").val());
+        $("#bananas").removeClass("d-none");
+        return false;
+    })
+
     });
