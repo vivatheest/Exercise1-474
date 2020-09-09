ef257947b1ec9600d6a50e844de68b68f0514e65
Viva Laas <vivalaas@udel.edu>
2020-09-09 12:12:47 -0400

    corrected the order of names

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(function(){
     //this code runs after page is fully loaded
     $("#go").on("click", function(event){
         event.preventDefault();
-        $("#bananas").text($("#fname").val() + " " + $("#lname").val());
+        $("#bananas").text($("#lname").val() + " " + $("#fname").val());
         $("#bananas").removeClass("d-none");
         return false;
     })
